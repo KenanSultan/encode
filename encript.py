c075975bb9d89502e6435aeb000cc1eab033cda4
KenanSultan <kenansultan@hotmail.com>
2019-04-30 14:01:20 +0400

    first improve version

--- encript.py
+++ encript.py
@@ -1,49 +1,27 @@
 import os
 
-letters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ !#$%&'()*+,-./:;<=>?@[]^_`{|}~" + '"' + "əƏöÖüÜıIğĞçÇşŞ"
-
-
-def encode(surf, file_name, *content):
+def encode(code, file_name, keys, *content):
     secure_content = []
     
     for i in content:
-        ind = letters.find(i)-surf
-        secure_content.append(letters[ind])
+        if keys.find(i) != -1:
+            ind = keys.find(i)-code%108
+            secure_content.append(keys[ind])
         
-    with open(file_name, "w") as file:
+    with open("secret_files/"+file_name, "w") as file:
         file.write("".join(secure_content))
 
-
-def decode(surf, file_name):
-    with open(file_name, "r") as file:
+def decode(code, keys, file_name):
+    with open("secret_files/"+file_name, "r") as file:
         secure_content = file.read()
 
     content = []
     
     for i in secure_content:
-        ind = letters.find(i)+surf
-        if ind > len(letters)-1:
-            ind = ind-len(letters)
-        content.append(letters[ind])
+        if keys.find(i) != '_':
+            ind = keys.find(i)+code%108
+            if ind > len(keys)-1:
+                ind = ind-len(keys)
+            content.append(keys[ind])
         
     return "".join(content)
-
-while True:
-    option = input("Sechim et [e, d, q]: ")
-
-    if option == 'e':
-        content = input("Metni girin: ")
-        file_name = input("Faylin adini sechin: ")+'.txt'
-        surf = int(input("Surushdurme sayi: "))
-        encode(surf, file_name, *content)
-    elif option == 'd':
-        print(", ".join(os.listdir()))
-        file_name = input("Faylin adini sechin: ")+'.txt'
-        surf = int(input("Surushdurme sayi: "))
-        print(decode(surf, file_name))
-    elif option == 'q':
-        print("Program sonlandi")
-        break
-    else:
-        print("Yanlish sechim")
-    
---
+++ main.py
@@ -0,0 +1,41 @@
+import inquirer
+from encript import *
+
+digits = '0123456789'
+letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+punct = "_ !#$%&'()*+,-./:;<=>?@[]^`{|}~" + '"'  + '\n'
+azeriLet = "əƏöÖüÜıİğĞçÇşŞ"
+
+all_keys = digits + letters + punct + azeriLet
+
+while True:
+    questions = [
+        inquirer.List ('opt',
+            message = "What you want? ",
+            choices = ['Encode the text','Decode the file','Quit'],
+        ),
+    ]
+    answer = inquirer.prompt(questions)['opt'][0]
+
+    if answer == 'E':
+        content = input("Write the text: ")
+        file_name = input("Write the file name: ")+'.txt'
+        code = int(input("Write the code: "))
+        encode(code, file_name, all_keys, *content)
+        print('\n')
+    elif answer == 'D':
+        options = [
+            inquirer.List ('opt',
+                message = "Choose file! ",
+                choices = [i for i in os.listdir('./secret_files/') if i[-4:] == '.txt'],
+            ),
+        ]
+        file_name = inquirer.prompt(options)['opt']
+        code = int(input("Write the code: "))
+        print(decode(code, all_keys, file_name))
+        print('\n')
+    elif answer == 'Q':
+        print("Bye\n")
+        exit()
+    else:
+        exit()
